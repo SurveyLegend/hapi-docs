7653b2482356e9de48abce8b9874a906f7a57932
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-01-31 12:33:23 +0100

    add sidebar

--- templates/js/patterns/Sidebar.vue
+++ templates/js/patterns/Sidebar.vue
@@ -1,19 +1,26 @@
 <template>
     <section class="sidebar">
+        <div class="sidebar__search h-relative">
+            <input class="sidebar__search__input" type="text" placeholder="Search">
+        </div>
         <ul>
-            <li>
-                <a href="#introduction">Introduction</a>
+            <li class="sidebar__item"><a class="sidebar__item__link" href="#introduction">Introduction</a></li>
+            <li class="sidebar__item"><a class="sidebar__item__link" href="#authentication">Authentication</a></li>
+            <li class="sidebar__item is-active">
+                <a class="sidebar__item__link" href="#kittens">Kittens</a>
+                <ul>
+                    <li class="sidebar__item"><a class="sidebar__item__link--sub is-active" href="#get-all-kittens">Get All Kittens</a></li>
+                    <li class="sidebar__item"><a class="sidebar__item__link--sub" href="#get-a-specific-kitten">Get a Specific Kitten</a></li>
+                    <li class="sidebar__item"><a class="sidebar__item__link--sub" href="#delete-a-specific-kitten">Delete a Specific Kitten</a></li>
+                </ul>
             </li>
-            <li>
-                <a href="#authentication">Authentication</a>
-            </li>
-            <li>
-                <a href="#kittens">Kittens</a>
-            </li>
-            <li>
-                <a href="#errors">Errors</a>
-            </li>
+            <li class="sidebar__item"><a class="sidebar__item__link" href="#errors">Errors</a></li>
         </ul>
+        <div class="sidebar__footer">
+            <ul>
+                <li class="sidebar__item"><a class="sidebar__item__link--footer" href="https://github.com/SurveyLegend/hapi-slate">Documentation Powered by Hapi Slate</a></li>
+            </ul>
+        </div>
     </section>
 </template>
 
