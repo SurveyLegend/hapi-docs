85f3c057a376c6b612f00b9bb958e07f4a447aa5
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-02-01 15:16:04 +0100

    add blockquotes

--- template/js/views/Main.vue
+++ template/js/views/Main.vue
@@ -4,15 +4,30 @@
             <h1 id="introduction">Introduction</h1>
             <p>Welcome to the Kittn API! You can use our API to access Kittn API endpoints, which can get information on various cats, kittens, and breeds in our database.</p>
             <p>We have language bindings in Shell, Ruby, Python and JavaScript! You can view code examples in the dark area to the right, and you can switch the programming language of the examples with the tabs in the top right.</p>
-            <p>This example API documentation page was created with <a class="h-text-underline" href="https://github.com/SurveyLegend/hapi-slate">Hapi Slate</a>. Feel free to edit it and use it as a base for your own API's documentation.</p>
+            <p>This example API documentation page was created with <a href="https://github.com/SurveyLegend/hapi-slate">Hapi Slate</a>. Feel free to edit it and use it as a base for your own API's documentation.</p>
 
             <h1 id="authentication">Authentication</h1>
-            <p>Kittn uses API keys to allow access to the API. You can register a new Kittn API key at our <a class="h-text-underline" href="http://example.com/developers">developer portal</a>.</p>
+
+            <blockquote>
+                <p>To authorize, use this code:</p>
+            </blockquote>
+
+            <blockquote>
+                <p>Make sure to replace <code>meowmeowmeow</code> with your API key.</p>
+            </blockquote>
+
+            <p>Kittn uses API keys to allow access to the API. You can register a new Kittn API key at our <a href="http://example.com/developers">developer portal</a>.</p>
             <p>Kittn expects for the API key to be included in all API requests to the server in a header that looks like the following:</p>
             <p><code>Authorization: meowmeowmeow</code></p>
             <notice type="info">You must replace <code>meowmeowmeow</code> with your personal API key.</notice>
 
-            <h1 id="kittens">Kittens</h1><h2 id="get-all-kittens">Get All Kittens</h2>
+            <h1 id="kittens">Kittens</h1>
+            <h2 id="get-all-kittens">Get All Kittens</h2>
+
+            <blockquote>
+                <p>The above command returns JSON structured like this:</p>
+            </blockquote>
+
             <p>This endpoint retrieves all kittens.</p>
             <h3 id="http-request">HTTP Request</h3>
             <p><code>GET http://example.com/api/kittens</code></p>
@@ -40,6 +55,11 @@
             </table>
             <notice type="success">Remember — a happy kitten is an authenticated kitten!</notice>
             <h2 id="get-a-specific-kitten">Get a Specific Kitten</h2>
+
+            <blockquote>
+                <p>The above command returns JSON structured like this:</p>
+            </blockquote>
+
             <p>This endpoint retrieves a specific kitten.</p>
 
             <notice type="error">Inside HTML code blocks like this one, you can't use Markdown, so use <code>&lt;code&gt;</code> blocks to denote code.</notice>
@@ -61,6 +81,11 @@
                 </tbody>
             </table>
             <h2 id="delete-a-specific-kitten">Delete a Specific Kitten</h2>
+
+            <blockquote>
+                <p>The above command returns JSON structured like this:</p>
+            </blockquote>
+
             <p>This endpoint deletes a specific kitten.</p>
             <h3 id="http-request-3">HTTP Request</h3>
             <p><code>DELETE http://example.com/kittens/{id}</code></p>
