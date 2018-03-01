ff6988ab1825365b2c79d1b21622844ce307872f
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-03-01 15:14:11 +0100

    add support for slugs & toc

--- template/js/app.js
+++ template/js/app.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
 import store from '@/js/store'
 import App from '@/js/layouts/App'
 
 Vue.config.productionTip = false
+
+Vue.use(VueScrollTo, {
+    container: '.content',
+    duration: -1
+})
+
+Vue.filter('capitalize', value => {
+    const string = value.toString()
+
+    return string.charAt(0).toUpperCase() + string.slice(1)
+})
 
 new Vue({
     store,
