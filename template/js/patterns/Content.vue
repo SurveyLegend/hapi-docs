6724a9047a85b8e943a2d5deabf6db5d980685da
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-03-04 22:56:30 +0100

    fix content prefix name

--- template/js/patterns/Content.vue
+++ template/js/patterns/Content.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="content">
         <template v-for="(group, groupName) in routes">
-            <section class="route" :id="groupName">
-                <div class="route__area">
-                    <div class="route__copy">
-                        <div class="route__copy__padding">
+            <section class="method first-of-group" :id="groupName">
+                <div class="method__area">
+                    <div class="method__copy">
+                        <div class="method__copy__padding">
                             <h1>{{ groupName | capitalize }}</h1>
                         </div>
                     </div>
                 </div>
             </section>
             <template v-for="route in group">
-                <section class="route" :id="route.slug">
-                    <div class="route__area">
-                        <div class="route__copy">
-                            <div class="route__copy__padding">
+                <section class="method" :id="route.slug">
+                    <div class="method__area">
+                        <div class="method__copy">
+                            <div class="method__copy__padding">
                                 <h1>{{ route.description }}</h1>
                                 <template v-if="route.notes">
                                     <p v-for="note in route.notes">
@@ -22,33 +22,33 @@
                                     </p>
                                 </template>
                             </div>
-                            <div class="route__list" :class="{ 'is-empty': route.pathParams === null }">
+                            <div class="method__list" :class="{ 'is-empty': route.pathParams === null }">
                                 <h5>Arguments</h5>
-                                <ul class="route__list__group">
+                                <ul class="method__list__group">
                                     <template v-if="route.pathParams !== null">
                                         <template v-for="param in route.pathParams.children">
-                                            <li class="route__list__item" :id="`${route.slug}-${param.name}`">
-                                                <h3 class="route__list__item__label">
+                                            <li class="method__list__item" :id="`${route.slug}-${param.name}`">
+                                                <h3 class="method__list__item__label">
                                                     <a class="header-anchor" :href="`#${route.slug}-${param.name}`" @click="scrollTo(`#${route.slug}-${param.name}`)"/>{{ param.name }}
-                                                    <span class="route__list__item__label__badge" v-if="param.flags && param.flags.required">Required</span>
-                                                    <span class="route__list__item__label__details" v-else>optional</span>
+                                                    <span class="method__list__item__label__badge" v-if="param.flags && param.flags.required">Required</span>
+                                                    <span class="method__list__item__label__details" v-else>optional</span>
                                                 </h3>
-                                                <vue-markdown :source="param.description" class="route__list__item__description"></vue-markdown>
+                                                <vue-markdown :source="param.description" class="method__list__item__description"></vue-markdown>
                                             </li>
                                         </template>
                                     </template>
-                                    <li class="route__list__item" v-else>
-                                        <h3 class="route__list__item__label">No arguments…</h3>
+                                    <li class="method__list__item" v-else>
+                                        <h3 class="method__list__item__label">No arguments…</h3>
                                     </li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="route__example">
-                            <div class="route__example__part">
-                                <div class="route__example__declaration">
+                        <div class="method__example">
+                            <div class="method__example__part">
+                                <div class="method__example__declaration">
                                     <prism language="bash">{{ route.method }} {{ route.uri + route.path }}</prism>
                                 </div>
-                                <!-- <div class="route__example__response">
+                                <!-- <div class="method__example__response">
                                     <prism language="json"></prism>
                                 </div> -->
                             </div>
