070aac3cf83a54782bd317fb88ee39691794a19b
Kiarokh <kiarokh@surveylegend.com>
2018-03-06 16:52:53 +0100

    new html structure and css for header, global layout changes

--- template/js/patterns/Header.vue
+++ template/js/patterns/Header.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="header">
-        <div class="header__sidebar">
+        <div class="header__section header__sidebar">
             <a class="header__logo" href="/"></a>
         </div>
-        <div class="header__copy"></div>
-        <div class="header__example"></div>
+        <div class="header__section header__area">
+            <div class="header__section header__copy"></div>
+            <div class="header__section header__example"></div>
+        </div>
     </div>
 </template>
 
