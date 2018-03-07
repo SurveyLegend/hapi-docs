ff51d216b1eece05a15314c3653c351c1d1b74ba
Kiarokh <kiarokh@surveylegend.com>
2018-03-07 15:39:02 +0100

    is-open animation for details of hamburgare-menu

--- template/js/patterns/Header.vue
+++ template/js/patterns/Header.vue
@@ -3,8 +3,8 @@
         <div class="header__section header__sidebar">
             <div class="header__hamburgar-menu">
                 <div class="hamburgar-menu__bread-top"></div>
-                <div  class="hamburgar-menu__hamburgar"></div>
-                <div  class="hamburgar-menu__hamburgar"></div>
+                <div  class="hamburgar-menu__hamburgar-slice-one"></div>
+                <div  class="hamburgar-menu__hamburgar-slice-two"></div>
                 <div class="hamburgar-menu__bread-bottom"></div>
             </div>
             <a class="header__logo" href="/"></a>
