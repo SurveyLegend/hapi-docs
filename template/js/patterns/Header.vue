eb2b451a83d5728390b0104f8ae152da864c1130
Robert Michalski <robert@surveylegend.com>
2018-08-14 11:56:46 +0200

    Pulled in some updates from dev branch.

--- template/js/patterns/Header.vue
+++ template/js/patterns/Header.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="header">
-        <div class="header__sidebar">
+        <div class="header__section header__sidebar">
+            <div class="header__hamburgar-menu">
+                <div class="hamburgar-menu__bread-top"></div>
+                <div  class="hamburgar-menu__hamburgar-slice-one"></div>
+                <div  class="hamburgar-menu__hamburgar-slice-two"></div>
+                <div class="hamburgar-menu__bread-bottom"></div>
+            </div>
             <a class="header__logo" href="/"></a>
         </div>
-        <div class="header__copy"></div>
-        <div class="header__example"></div>
+        <div class="header__section header__area">
+            <div class="header__section header__copy"></div>
+            <div class="header__section header__example"></div>
+        </div>
     </div>
 </template>
 
