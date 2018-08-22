87aff642be206cf22474a543647e4e9ecafc4c35
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-08-22 15:55:29 +0200

    restore frontend updates

--- template/js/patterns/Header.vue
+++ template/js/patterns/Header.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="header">
-        <div class="header__sidebar">
+        <div class="header__section header__sidebar">
+            <div class="header__hamburgar-menu">
+                <div class="hamburgar-menu__bread-top"/>
+                <div class="hamburgar-menu__hamburgar-slice-one"/>
+                <div class="hamburgar-menu__hamburgar-slice-two"/>
+                <div class="hamburgar-menu__bread-bottom"/>
+            </div>
             <a
                 class="header__logo"
-                href="/"
-            />
+                href="/"/>
         </div>
-        <div class="header__copy"/>
-        <div class="header__example"/>
+        <div class="header__section header__area">
+            <div class="header__section header__copy"/>
+            <div class="header__section header__example"/>
+        </div>
     </div>
 </template>
 
