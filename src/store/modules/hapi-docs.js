b21ea76f430e763c82bf519a2077c4b596fa0d2c
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-10-18 17:48:48 +0200

    improve grouped paths logic

--- src/store/modules/hapi-docs.js
+++ src/store/modules/hapi-docs.js
@@ -1,42 +1,41 @@
 import * as types from '../mutation-types'
 
 export const state = {
-    data: {}
+    info: {},
+    paths: [],
+    groups: [],
+    tags: []
 }
 
 export const getters = {
-    info: state => state.data.info,
-    paths: state => state.data.paths,
-    groups: state => state.data.groups,
-    tags: state => state.data.tags,
+    info: state => state.info,
+    paths: state => state.paths,
+    groups: state => state.groups,
+    tags: state => state.tags,
     groupedPaths: (state, getters) => {
-        const groups = []
+        const grouped = []
 
-        if (getters.tags) {
-            getters.tags.forEach(tag => {
-                groups.push({
-                    name: tag.name,
-                    description: tag.description,
-                    deprecated: tag.deprecated,
-                    paths: getters.paths.filter(path => path.group === tag.name)
-                })
-            })
-        } else if (getters.groups) {
-            getters.groups.forEach(group => {
-                groups.push({
-                    name: group,
-                    paths: getters.paths.filter(path => path.group === group)
-                })
-            })
-        }
+        getters.groups.forEach(group => {
+            const tag = getters.tags.find(tag => tag.name === group)
 
-        return groups
+            grouped.push({
+                name: group,
+                description: tag.description || null,
+                deprecated: tag.deprecated || false,
+                paths: getters.paths.filter(path => path.group === group)
+            })
+        })
+
+        return grouped
     }
 }
 
 export const mutations = {
-    [types.FETCH_DATA](state, { hapiDocs }) {
-        state.data = hapiDocs
+    [types.FETCH_DATA](state, { data }) {
+        state.info = data.info
+        state.paths = data.paths
+        state.groups = data.groups
+        state.tags = data.tags
     }
 }
 
@@ -44,6 +43,6 @@ export const actions = {
     async fetchData({ commit }) {
         const { data } = await this.$axios.get('/hapi-docs')
 
-        commit(types.FETCH_DATA, { hapiDocs: data })
+        commit(types.FETCH_DATA, { data })
     }
 }
