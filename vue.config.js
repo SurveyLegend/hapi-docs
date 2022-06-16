c6b5afa34196db42ccc7516a0320c678ed2b0f7f
Robert Michalski <robert@surveylegend.com>
2022-06-16 12:55:55 +0200

    Linting fixes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+    // baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     devServer: {
         port: 3001,
